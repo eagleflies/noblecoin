2c4a0a87cb7403aab32738de2000d78f9f8176ea
Eagle Flies <myeagleflies@gmail.com>
2015-02-17 11:07:46 +0100

    Changing transaction fee to 1 and bumping version number.

--- src/version.h
+++ src/version.h
@@ -25,10 +25,10 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 88883;
+static const int PROTOCOL_VERSION = 88884;
 
-// earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 88883;
+// earlier versions not supported 
+static const int MIN_PROTO_VERSION = 88884;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
