a07dbabe82ae5e49736106671bb37ad83a1bf145
Eagle Flies <myeagleflies@gmail.com>
2015-03-02 12:49:55 +0100

    Bumping version number again. We will use 89xxx for prod network.

--- src/version.h
+++ src/version.h
@@ -25,10 +25,10 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 88885;
+static const int PROTOCOL_VERSION = 89000;
 
 // earlier versions not supported 
-static const int MIN_PROTO_VERSION = 88885;
+static const int MIN_PROTO_VERSION = 89000;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
